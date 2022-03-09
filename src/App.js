54bb4f0fd6007ff8b72952112bf27c6f82ae91ea
DeveloperYamin <developer.yamin@gmail.com>
2022-03-09 15:01:37 +0600

    integration react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { Routes } from "react-router-dom";
+import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 
 
 function App() {
   return (
-    <div className="">
+
+    <div className="app">
       <Header />
-      <Sidebar />
+      <main className="app_body" >
+        <Sidebar />
+        <Routes>
+        </Routes>
+      </main>
     </div>
+
   );
 }
 

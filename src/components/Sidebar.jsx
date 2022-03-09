66418189b77332a4ce9cef098c50c9080a3f79ac
DeveloperYamin <developer.yamin@gmail.com>
2022-03-09 14:35:23 +0600

    used SidebarOption

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,16 @@
-import { Add, Inbox, } from "@mui/icons-material";
+import {
+  AccessTime,
+  Add,
+  ExpandMore,
+  Inbox,
+  LabelImportant,
+  NearMe,
+  Note,
+  Star,
+} from "@mui/icons-material";
 import { Button } from "@mui/material";
 import "../styles/Sidebar.css";
-import HeaderOption from "./SidebarOption";
+import SidebarOption from "./SidebarOption";
 
 function Sidebar() {
   return (
@@ -9,7 +18,13 @@ function Sidebar() {
       <Button startIcon={<Add fontSize="large" />} className="sidebar_compose">
         Compose
       </Button>
-      <HeaderOption Icon={Inbox} title="Inbox" number={54} />
+      <SidebarOption Icon={Inbox} title="Inbox" number={54} selected />
+      <SidebarOption Icon={Star} title="Starred" number={46} />
+      <SidebarOption Icon={AccessTime} title="Snoozed" number={34} />
+      <SidebarOption Icon={LabelImportant} title="Important" number={78} />
+      <SidebarOption Icon={NearMe} title="Sent" number={64} />
+      <SidebarOption Icon={Note} title="Drafts" number={58} />
+      <SidebarOption Icon={ExpandMore} title="More" number={48} />
     </section>
   );
 }
--- src/components/SidebarOption.jsx
+++ src/components/SidebarOption.jsx
@@ -1,7 +1,11 @@
 import "../styles/SidebarOption.css";
 
-function SidebarOption({Icon,title,number}) {
-  return <div>SidebarOption</div>;
+function SidebarOption({Icon,title,number,selected}) {
+  return <div className={`sidebarOption ${ selected && "sidebarOption--active"} `} >
+    <Icon />
+    <h3>{title}</h3>
+    <p>{number}</p>
+  </div>;
 }
 
 export default SidebarOption;

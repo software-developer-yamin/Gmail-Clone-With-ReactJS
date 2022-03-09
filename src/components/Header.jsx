f51eb4a68f97386e85b4c366dd1b06b780d42345
DeveloperYamin <developer.yamin@gmail.com>
2022-03-09 09:40:11 +0600

    integrate middle section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Menu } from "@mui/icons-material";
+import { ArrowDropDown, Menu, Search } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
 import "../styles/Header.css";
 
@@ -13,6 +13,11 @@ function Header() {
           src="https://cdn.vox-cdn.com/thumbor/8fWz6qpiMYMsZhY4vrc9Vhl5yL8=/0x110:1320x770/fit-in/1200x600/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg"
           alt=""
         />
+      </section>
+      <section className="header_middle">
+        <Search />
+        <input placeholder="Search Mail" type="text" />
+        <ArrowDropDown className="header_inputCaret" />
       </section>
     </header>
   );

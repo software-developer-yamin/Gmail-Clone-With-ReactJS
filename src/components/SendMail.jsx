4328865ebbd64be02d60ee311e4bddf5730a2490
DeveloperYamin <developer.yamin@gmail.com>
2022-03-10 20:48:03 +0600

    include form validation functionality

--- src/components/SendMail.jsx
+++ src/components/SendMail.jsx
@@ -1,10 +1,21 @@
 import { Close } from "@mui/icons-material";
-import { Button, IconButton, Input } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
+import { useForm } from "react-hook-form";
 import "../styles/SendMail.css";
 
 function SendMail() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch("example")); // watch input value by passing the name of it
+
   return (
-    <section className="sendMail">
+    <section onSubmit={handleSubmit(onSubmit)} className="sendMail">
       <header className="sendMail_header">
         <h3>New Message</h3>
         <IconButton>
@@ -12,12 +23,36 @@ function SendMail() {
         </IconButton>
       </header>
       <form>
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        
+        <input
+          placeholder="To"
+          type="text"
+          {...register("to", { required: true })}
+        />
+        {errors.to && <span className="sendMail_error">To is required!</span>}
+        <input
+          placeholder="Subject"
+          type="text"
+          {...register("subject", { required: true })}
+        />
+        {errors.to && (
+          <span className="sendMail_error">Subject is required!</span>
+        )}
+        <input
+          className="sendMail_message"
+          placeholder="Message..."
+          type="text"
+          {...register("message", { required: true })}
+        />
+        {errors.to && <span className="sendMail_error">Message is required!</span>}
         <div className="sendMail_options">
-          <Button>Send</Button>
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            className="sendMail_send"
+          >
+            Send
+          </Button>
         </div>
       </form>
     </section>

9b435a11e7aeed7606c3d9cbe47a1a094b92cfc3
DeveloperYamin <developer.yamin@gmail.com>
2022-03-11 12:06:51 +0600

    create build folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,26 @@
-import { Apps, ArrowDropDown, Menu, Notifications, Search, } from "@mui/icons-material";
+import {
+  Apps,
+  ArrowDropDown,
+  Menu,
+  Notifications,
+  Search,
+} from "@mui/icons-material";
 import { Avatar, IconButton } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectUser } from "../features/user/userSlice";
+import { auth } from "../firebase";
 import "../styles/Header.css";
 
 function Header() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  const logOut = () => {
+    auth.signOut().then(() => {
+      dispatch(logout());
+    });
+  };
+
   return (
     <header className="header">
       <section className="header_left">
@@ -19,14 +37,16 @@ function Header() {
         <input placeholder="Search mail" type="text" />
         <ArrowDropDown className="header_inputCaret" />
       </section>
-      <section className="header_right" >
+      <section className="header_right">
         <IconButton>
           <Apps />
         </IconButton>
         <IconButton>
           <Notifications />
         </IconButton>
-        <Avatar />
+        <IconButton>
+          <Avatar onClick={logOut} src={user?.photoUrl} />
+        </IconButton>
       </section>
     </header>
   );
